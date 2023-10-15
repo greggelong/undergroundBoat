d92d3370538c1b29e2667b10db9d0c7e7ec938a8
Greg K <gkreisman_cep@qq.com>
2023-10-15 17:39:16 +0800

    change to minifiy and take out os try

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let boat
-let osphone = false;
-let androidphone = false;
+//let osphone = false;
+//let androidphone = false;
 let x, y,z, xx, zz
 let bsz
 let yarr = []
@@ -21,17 +21,17 @@ function setup() {
   sz =width/200
   imageMode(CENTER)
   // added code for os phones
-  if (typeof DeviceMotionEvent.requestPermission === 'function') {
-    DeviceMotionEvent.requestPermission()
-      .then(permissionState => {
-        if (permissionState === 'granted') {
-          osphone=true;
-        }
-      })
-      .catch(console.error);
-  } else {
-    androidphone = true;
-  }
+  // if (typeof DeviceMotionEvent.requestPermission === 'function') {
+  //   DeviceMotionEvent.requestPermission()
+  //     .then(permissionState => {
+  //       if (permissionState === 'granted') {
+  //         osphone=true;
+  //       }
+  //     })
+  //     .catch(console.error);
+  // } else {
+  //   androidphone = true;
+  // }
 }
 
 function draw() {
