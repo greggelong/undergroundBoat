56d41f4920817c9a443873932e4a852dfb8d5fd3
Greg K <gkreisman_cep@qq.com>
2024-07-02 20:43:57 +0800

    add camera to underground boat

---
+++ sketch1.js
@@ -0,0 +1,140 @@
+let boat
+let permissionGranted = false;
+//let osphone = false;
+//let androidphone = false;
+let x, y,z, xx, zz
+let bsz
+let yarr = []
+let xarr =[]
+let zarr =[]
+let sz
+let capture
+
+
+function preload(){
+  boat = loadImage("oboats.png")
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(1)
+  bsz = windowWidth/2
+  boat.resize(bsz,0)
+  x=0
+  sz =width/200
+  var constraints = {
+    audio: false,
+    video: {
+      facingMode: {
+        exact: "environment",
+      },
+    },
+    //video: {
+    //facingMode: "user"
+    //}
+  };
+  capture = createCapture(VIDEO, constraints);
+  capture.size(width/10, height/10);
+  capture.hide();
+  imageMode(CENTER)
+  // added code for os phones
+  if(typeof(DeviceOrientationEvent) !== 'undefined' && typeof (DeviceOrientationEvent.requestPermission) === 'function'){
+    // ios 13 device
+    DeviceOrientationEvent.requestPermission()
+    .catch(() => {
+      // show permissionn dialog only the first time
+      let button = createButton("click to allow access to sensors");
+      button.style("font-size", "24px");
+      button.center();
+      button.mousePressed(requestAccess);
+      throw error;
+    })
+    .then(()=>{
+     // on any subsequent visit
+     permissionGranted = true;
+    })
+  } else{
+    // non ios devise
+    // grant access anyway
+    console.log("went through the permessions checkcheck")
+    permissionGranted = true;
+  }
+}
+
+function requestAccess(){
+  DeviceOrientationEvent.requestPermission()
+    .then(response =>{
+      if (response == 'granted'){
+        permissionGranted = true;
+      } else{
+        permissionGranted = false;
+      }
+    })
+    .catch(console.error)
+    // remove the button
+    this.remove()
+}
+
+function draw() {
+  image(capture,0,0,width,height)
+  // y sky
+  fill(0,0,130)
+  angleMode(DEGREES)
+  xx =accelerationX*10
+  zz =accelerationZ*10
+  y = accelerationY*10;
+  yarr.unshift(y)
+  zarr.unshift(zz)
+  xarr.unshift(xx)
+  if (yarr.length>200){
+    yarr.pop()
+    
+  }
+  stroke(255,255,0)
+  strokeWeight(5)
+  noFill();
+// y line
+  beginShape()
+  for (let i =0; i<yarr.length; i++){
+    //ellipse(i*sz,height/4+yarr[i],30,30 )
+    vertex(i*sz,height/4+yarr[i])
+  }
+  endShape()
+  // xline
+  stroke(0,0,255)
+  beginShape()
+
+  for (let i =0; i<xarr.length; i++){
+
+    //ellipse(i*sz,height/4+yarr[i],30,30 )
+
+    vertex(i*sz,height/2+xarr[i])
+
+  }
+
+  endShape()
+  // z line
+  beginShape()
+  stroke(0)
+  for (let i =0; i<zarr.length; i++){
+    //ellipse(i*sz,height/4+yarr[i],30,30 )
+    vertex(i*sz,height/1.2+zarr[i])
+  }
+  endShape()
+
+
+  
+  
+  
+  push();
+  translate(width/2,height/2)
+  x=accelerationX*4;
+    z = accelerationZ*0.05;
+
+  scale(1+z)
+  rotate(x)
+  image(boat,0,0);
+  pop()
+  
+  
+}
